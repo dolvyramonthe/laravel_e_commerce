5f59a5876e437660db26abe6f83447a0950b8448
Mamadou Dian DIALLO <diallo_z@etna-alternance.net>
2023-11-14 12:20:31 +0100

    diallo_z project setup

---
+++ laravel_e_commerce/database/migrations/2023_11_14_100553_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->String('name', 100)->unique();
+            $table->text('desc');
+            $table->float('price');
+            $table->string('image_path')->nullable();
+            $table->boolean('is_available')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ laravel_e_commerce/resources/views/welcome.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.head')
+ 
+@section('main')
+    <h1>Welcome to the best e-commerce app</h1>
+@endsection
