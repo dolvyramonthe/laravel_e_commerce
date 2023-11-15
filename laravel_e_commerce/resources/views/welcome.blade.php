eea3169316dc8c67b770a350f0fa158cd519f0ac
kayama_c <kayama_c@debian>
2023-11-15 14:04:28 +0100

    Login.blade.css

--- laravel_e_commerce/resources/views/welcome.blade.php
+++ laravel_e_commerce/resources/views/welcome.blade.php
@@ -1,7 +1,23 @@
 @extends('layouts.header')
 
 @section('content')
-<div>
+<div style="padding: 20px; background-color: #f4f4f4;">
+    <style>
+            
+            h1 {
+                color: #0066cc;
+                font-size: 28px;
+                text-align: center;
+            }
+
+            p {
+                color: #333;
+                font-size: 16px;
+                text-align: center;
+            }
+            
+            /* Ajoutez d'autres styles selon vos besoins */
+        </style>
     <h1>Welcome to the BubbleMyTea App</h1>
     <p>Please login to continue:</p>
 </div>
