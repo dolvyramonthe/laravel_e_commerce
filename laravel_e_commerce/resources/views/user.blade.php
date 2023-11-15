05793334aa2f5c549535114b1ef69b37d09d5171
Mamadou Dian DIALLO <diallo_z@etna-alternance.net>
2023-11-15 17:17:44 +0100

    user profile avatar to home page

--- laravel_e_commerce/resources/views/user.blade.php
+++ laravel_e_commerce/resources/views/user.blade.php
@@ -4,6 +4,11 @@
     <div>
         @auth
             <h1>Welcome, {{ auth()->user()->name }}!</h1>
+            <div class="user-avatar">
+                @if(auth()->user()->avatar)
+                    <img src="{{ asset('storage/' . auth()->user()->avatar) }}" alt="User Avatar" style="max-width: 100px; max-height: 100px;">
+                @endif
+            </div>
             <p>Manage your profile:</p>
             <ul>
                 <li><a href="{{ route('profile') }}">Update Profile</a></li>
