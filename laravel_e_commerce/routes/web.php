e0dbe434f29a7ba12d82c67c4b7c2a1a02ed9ab1
roseline <monthe_r@etna-alternance.net>
2023-11-17 10:20:01 +0100

    manage product

--- laravel_e_commerce/routes/web.php
+++ laravel_e_commerce/routes/web.php
@@ -17,29 +17,6 @@ use App\Http\Controllers\ProductController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-// Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-// Route::post('/login', [LoginController::class, 'login']);
-// Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-// Route::get('/admin', function () {
-//     return view('admin');
-// })->name('admin')->middleware('auth');
-
-// Route::get('/user', function () {
-//     return view('user');
-// })->name('user')->middleware('auth');
-
-// // Additional middleware-based routes for admin and user
-// Route::get('/admin', 'AdminController@index')->middleware('admin');
-// Route::get('/user', 'UserController@index')->middleware('user');
-
-// Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-// Route::post('/register', [RegisterController::class, 'register']);
-
 Route::get('/', function () {
     return view('welcome');
 });
