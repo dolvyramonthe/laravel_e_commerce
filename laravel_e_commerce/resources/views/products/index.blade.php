55b68493377ac605b803b3803b8048bc0e2c25c9
Mamadou Dian DIALLO <diallo_z@etna-alternance.net>
2023-11-17 11:16:04 +0100

    products update an delete  management

--- laravel_e_commerce/resources/views/products/index.blade.php
+++ laravel_e_commerce/resources/views/products/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.header')
+{{-- @extends('layouts.header')
 
 @section('content')
     <h1>Liste des Produits</h1>
@@ -24,5 +24,70 @@
             @endif
         </div>
     @endforeach
+@endsection --}}
+
+@extends('layouts.header')
+
+@section('content')
+    <div>
+        @auth
+        <h1>Liste des Produits</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Price</th>
+                        <th>Availability</th>
+                        <th>Poppings</th>
+                        <th>Sugar Quantity</th>
+                        <th>Size</th>
+                        <th>Product Image</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($products as $product)
+                        <tr>
+                            <td>{{ $product->name }}</td>
+                            <td>{{ $product->desc }}</td>
+                            <td>{{ $product->price }}</td>
+                            <td>{{ $product->is_available === 1 ? "Available" : "Not Available" }}</td>
+                            <td>{{ $product->poppings }}</td>
+                            <td>{{ $product->sugar_quantity }}</td>
+                            <td>{{ $product->size }}</td>
+                            <td>
+                                @if ($product->image_path)
+                                    <a href="{{ asset('storage/' . $product->image_path) }}" target="_blank">
+                                        <img src="{{ asset('storage/' . $product->image_path) }}" alt="{{ $product->name }}" style="max-width: 200px; max-height: 200px;">
+                                    </a>
+                                @else
+                                    <p>No image available</p>
+                                @endif
+                            </td>
+                            <td>
+                                <a href="{{ route('products.edit', $product->id) }}">Update</a>
+                                <form id="delete-form-{{ $product->id }}" method="POST" action="{{ route('products.destroy', $product->id) }}">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="button" onclick="confirmDelete({{ $product->id }})">Delete</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <a href="{{ route('addadmin') }}">+</a>
+        @endauth
+    </div>
+
+    <script>
+        function confirmDelete(userId) {
+            if (confirm('Are you sure you want to delete this user?')) {
+                document.getElementById('delete-form-' + userId).submit();
+            }
+        }
+    </script>
 @endsection
+
 
