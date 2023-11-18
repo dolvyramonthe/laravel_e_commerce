71bbb81b621b72b4f5bd28c1d4e4084813bcacc9
Mamadou Dian DIALLO <diallo_z@etna-alternance.net>
2023-11-18 13:40:38 +0100

    add order management css

--- laravel_e_commerce/resources/views/orders/show.blade.php
+++ laravel_e_commerce/resources/views/orders/show.blade.php
@@ -1,12 +1,20 @@
 @extends('layouts.header')
 
 @section('content')
-    <div class="container">
-        <h1>Order Details - ID: {{ $order->id }}</h1>
-        <p>Status: {{ $order->status }}</p>
-        <p>Total Amount: {{ $order->total_amount }}</p>
 
-        <h2>Ordered Products</h2>
+    <style>
+         li {
+            display: flex;
+            align-items: center;
+            margin-bottom: 5px;
+        }
+    </style>
+    <div class="container">
+        <h1 style="font-size: 24px; color: #fff; margin-bottom: 20px;">Order Details - ID: {{ $order->id }}</h1>
+        <p style="font-size: 24px; color: #333; margin-bottom: 20px;">Status: {{ $order->status }}</p>
+        <p style="font-size: 24px; color: #333; margin-bottom: 20px;">Total Amount: {{ $order->total_amount }}</p>
+
+        <h2 style="font-size: 24px; color: #fff; margin-bottom: 20px;">Ordered Products</h2>
         <ul>
             @foreach ($order->products as $product)
                 <li>
@@ -29,12 +37,12 @@
                     @if ($product->image_path)
                         <img src="{{ asset('storage/' . $product->image_path) }}" alt="{{ $product->name }} Image">
                     @else
-                        <p>No image available</p>
+                        <p style="font-size: 24px; color: #fff; margin-bottom: 20px;">No image available</p>
                     @endif
                 </li>
             @endforeach
         </ul>
 
-        <a href="{{ route('orders.index') }}" class="btn btn-primary">Back to Orders</a>
+        <a href="{{ route('orders.index') }}" class="btn btn-primary" style="text-decoration: none; display: block; color: violet; margin-top: 20px; font-size: 20px;">Back to Orders</a>
     </div>
 @endsection
